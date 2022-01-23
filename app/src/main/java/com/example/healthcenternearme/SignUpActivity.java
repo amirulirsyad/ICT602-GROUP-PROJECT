2b90ef47f1d3adef0c6ea5b99cc386d75608d03d
amirulirsyad <2020489528@student.uitm.edu.my>
2022-01-23 14:38:35 +0800

    login signup

--- app/src/main/java/com/example/healthcenternearme/SignUpActivity.java
+++ app/src/main/java/com/example/healthcenternearme/SignUpActivity.java
@@ -1,30 +1,31 @@
 package com.example.healthcenternearme;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
-    EditText etEmail, etPassword;
+    EditText etFirstName, etLastName, etEmail, etPassword, etRepeatPassword;
     final int MIN_PASSWORD_LENGTH = 6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
 
         viewInitializations();
     }
 
     void viewInitializations() {
+        etFirstName = findViewById(R.id.et_first_name);
+        etLastName = findViewById(R.id.et_last_name);
         etEmail = findViewById(R.id.et_email);
         etPassword = findViewById(R.id.et_password);
+        etRepeatPassword = findViewById(R.id.et_repeat_password);
 
         // To show back button in actionbar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -32,13 +33,24 @@ public class LoginActivity extends AppCompatActivity {
 
     // Checking if the input in form is valid
     boolean validateInput() {
-
+        if (etFirstName.getText().toString().equals("")) {
+            etFirstName.setError("Please Enter First Name");
+            return false;
+        }
+        if (etLastName.getText().toString().equals("")) {
+            etLastName.setError("Please Enter Last Name");
+            return false;
+        }
         if (etEmail.getText().toString().equals("")) {
             etEmail.setError("Please Enter Email");
             return false;
         }
         if (etPassword.getText().toString().equals("")) {
             etPassword.setError("Please Enter Password");
+            return false;
+        }
+        if (etRepeatPassword.getText().toString().equals("")) {
+            etRepeatPassword.setError("Please Enter Repeat Password");
             return false;
         }
 
@@ -54,6 +66,11 @@ public class LoginActivity extends AppCompatActivity {
             return false;
         }
 
+        // Checking if repeat password is same
+        if (!etPassword.getText().toString().equals(etRepeatPassword.getText().toString())) {
+            etRepeatPassword.setError("Password does not match");
+            return false;
+        }
         return true;
     }
 
@@ -68,18 +85,16 @@ public class LoginActivity extends AppCompatActivity {
 
             // Input is valid, here send data to your server
 
+            String firstName = etFirstName.getText().toString();
+            String lastName = etLastName.getText().toString();
             String email = etEmail.getText().toString();
             String password = etPassword.getText().toString();
+            String repeatPassword = etRepeatPassword.getText().toString();
 
             Toast.makeText(this,"Login Success",Toast.LENGTH_SHORT).show();
             // Here you can call you API
-            // Check this tutorial to call server api through Google Volley Library https://handyopinion.com
+
         }
     }
 
-    public void goToSignup(View v) {
-
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
-    }
 }
